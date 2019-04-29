733a21bee66a7d86db87bd914c15b863f9e7fad2
Emerson de Almeida <kharitonov9000@gmail.com>
2019-04-29 07:14:38 -0300

    enviando js

---
+++ js/anima.js
@@ -0,0 +1,166 @@
+$(function(){
+
+var animacao = "animated flip";
+var finanimacao = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+
+	$(".acao").hover(function(e) {
+
+	$(".recebe").addClass(animacao).one(finanimacao, function(){
+
+		$(this).removeClass(animacao);
+
+		});
+
+		e.preventDefault();
+   });
+
+});
+
+$(function(){
+
+var animacao = "animated flip";
+var finanimacao = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+
+	$(".a").hover(function(e) {
+
+	$(".b").addClass(animacao).one(finanimacao, function(){
+
+		$(this).removeClass(animacao);
+
+		});
+
+		e.preventDefault();
+   });
+
+});
+
+$(function(){
+
+var animacao = "animated flip";
+var finanimacao = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+
+	$(".c").hover(function(e) {
+
+	$(".d").addClass(animacao).one(finanimacao, function(){
+
+		$(this).removeClass(animacao);
+
+		});
+
+		e.preventDefault();
+   });
+
+});
+
+
+$(function(){
+
+var animacao = "animated flip";
+var finanimacao = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+
+	$(".e").hover(function(e) {
+
+	$(".f").addClass(animacao).one(finanimacao, function(){
+
+		$(this).removeClass(animacao);
+
+		});
+
+		e.preventDefault();
+   });
+
+});
+
+
+$(function(){
+
+var animacao = "animated flip";
+var finanimacao = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+
+	$(".g").hover(function(e) {
+
+	$(".h").addClass(animacao).one(finanimacao, function(){
+
+		$(this).removeClass(animacao);
+
+		});
+
+		e.preventDefault();
+   });
+
+});
+
+$(function(){
+
+var animacao = "animated flip";
+var finanimacao = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+
+	$(".i").hover(function(e) {
+
+	$(".j").addClass(animacao).one(finanimacao, function(){
+
+		$(this).removeClass(animacao);
+
+		});
+
+		e.preventDefault();
+   });
+
+});
+
+
+$(function(){
+
+var animacao = "animated flip";
+var finanimacao = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+
+	$(".k").hover(function(e) {
+
+	$(".l").addClass(animacao).one(finanimacao, function(){
+
+		$(this).removeClass(animacao);
+
+		});
+
+		e.preventDefault();
+   });
+
+});
+
+
+$(function(){
+
+var animacao = "animated flip";
+var finanimacao = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+
+	$(".m").hover(function(e) {
+
+	$(".n").addClass(animacao).one(finanimacao, function(){
+
+		$(this).removeClass(animacao);
+
+		});
+
+		e.preventDefault();
+   });
+
+});
+
+
+$(function(){
+
+var animacao = "animated flipY";
+var finanimacao = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+
+	$(".mdb").hover(function(e) {
+
+	$(".mdj").addClass(animacao).one(finanimacao, function(){
+
+		$(this).removeClass(animacao);
+
+		});
+
+		e.preventDefault();
+   });
+
+});
